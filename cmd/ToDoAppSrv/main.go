770d3973a08ed21a2bd39143d5a84a23302404e2
melonsoda20 <benedictusteddy200596@gmail.com>
2022-04-07 12:37:13 +0700

    Split up the files for routing

--- cmd/ToDoAppSrv/main.go
+++ cmd/ToDoAppSrv/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"net/http"
+	"ToDoApp/internal/handler/hello"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", hello.GetHello)
 	e.Logger.Fatal(e.Start(":1323"))
 }
